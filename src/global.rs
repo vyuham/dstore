0a9e4fd8ef9582c52bf27e1abcb6d3d62a86d9e6
Devdutt Shenoi <devdutt@outlook.in>
2021-05-17 18:54:05 +0530

    general fixing and maintanence

--- src/global.rs
+++ src/global.rs
@@ -2,11 +2,10 @@ use bytes::Bytes;
 use futures::StreamExt;
 use std::{
     collections::{HashMap, VecDeque},
-    net::SocketAddr,
     str,
-    sync::{Arc, Mutex},
+    sync::Arc,
 };
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, Mutex};
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{transport::Server, Request, Response, Status};
 
@@ -23,7 +22,7 @@ pub struct Global {
     /// In-memory database mapping KEY -> VALUE
     db: Arc<Mutex<HashMap<Bytes, Bytes>>>,
     /// Maps Local UIDs to a KEY invalidation queue
-    cluster: Arc<Mutex<HashMap<Bytes, Arc<Mutex<VecDeque<Bytes>>>>>>,
+    cluster: Arc<Mutex<HashMap<Bytes, Mutex<VecDeque<Bytes>>>>>,
 }
 
 impl Global {
@@ -36,10 +35,10 @@ impl Global {
     }
 
     /// Initialiaze server and start Global service on `addr`
-    pub async fn start_server(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn start_server(addr: &str) -> Result<(), Box<dyn std::error::Error>> {
         Server::builder()
             .add_service(DstoreServer::new(Self::new()))
-            .serve(addr)
+            .serve(addr.parse().unwrap())
             .await?;
 
         Ok(())
@@ -50,9 +49,9 @@ impl Global {
 impl Dstore for Global {
     /// RPC to add new Local to cluster, with empty invalidation queue
     async fn join(&self, args: Request<Byte>) -> Result<Response<Null>, Status> {
-        self.cluster.lock().unwrap().insert(
+        self.cluster.lock().await.insert(
             Bytes::from(args.into_inner().body),
-            Arc::new(Mutex::new(VecDeque::new())),
+            Mutex::new(VecDeque::new()),
         );
 
         Ok(Response::new(Null {}))
@@ -60,7 +59,7 @@ impl Dstore for Global {
 
     /// Check if a certain KEY exists on Global, if yes return size of associated VALUE
     async fn contains(&self, args: Request<Byte>) -> Result<Response<Size>, Status> {
-        match self.db.lock().unwrap().get(&args.into_inner().body[..]) {
+        match self.db.lock().await.get(&args.into_inner().body[..]) {
             Some(value) => Ok(Response::new(Size {
                 size: value.len() as i32,
             })),
@@ -70,15 +69,15 @@ impl Dstore for Global {
 
     /// RPC that maps KEY to VALUE, if it doesn't already exist on Global
     async fn push(&self, args: Request<KeyValue>) -> Result<Response<Null>, Status> {
-        let mut db = self.db.lock().unwrap();
-        let args = args.into_inner();
-        match db.contains_key(&args.key[..]) {
+        let mut db = self.db.lock().await;
+        let KeyValue { key, value } = args.into_inner();
+        match db.contains_key(&key[..]) {
             true => Err(Status::already_exists(format!(
                 "{} already in use.",
-                str::from_utf8(&args.key).unwrap()
+                str::from_utf8(&key).unwrap()
             ))),
             false => {
-                db.insert(Bytes::from(args.key), Bytes::from(args.value));
+                db.insert(Bytes::from(key), Bytes::from(value));
                 Ok(Response::new(Null {}))
             }
         }
@@ -94,18 +93,18 @@ impl Dstore for Global {
         let mut i = 0;
         let (mut key, mut buf) = (vec![], vec![]);
         while let Some(byte) = stream.next().await {
-            let byte = byte?;
+            let Byte { body } = byte?;
             if i == 0 {
-                key.append(&mut byte.body.clone());
+                key.append(&mut body.clone());
             } else {
-                buf.append(&mut byte.body.clone());
+                buf.append(&mut body.clone());
             }
             i += 1;
         }
 
         self.db
             .lock()
-            .unwrap()
+            .await
             .insert(Bytes::from(key), Bytes::from(buf));
 
         Ok(Response::new(Null {}))
@@ -113,13 +112,13 @@ impl Dstore for Global {
 
     /// RPC that returns VALUE associated with KEY, provided it exist on Global
     async fn pull(&self, args: Request<Byte>) -> Result<Response<Byte>, Status> {
-        let db = self.db.lock().unwrap();
-        let args = args.into_inner();
-        match db.get(&args.body[..]) {
+        let db = self.db.lock().await;
+        let Byte { body } = args.into_inner();
+        match db.get(&body[..]) {
             Some(val) => Ok(Response::new(Byte { body: val.to_vec() })),
             None => Err(Status::not_found(format!(
                 "{} mapping doesn't exist.",
-                str::from_utf8(&args.body).unwrap()
+                str::from_utf8(&body).unwrap()
             ))),
         }
     }
@@ -135,15 +134,11 @@ impl Dstore for Global {
         // Create a double ended channel for transporting VALUE packets processed within thread
         let (tx, rx) = mpsc::channel(4);
         let db = self.db.clone();
+        let Byte { body } = args.into_inner();
 
         // Spawn thread to manage partitioning of a large VALUE into packet frames
         tokio::spawn(async move {
-            let val = db
-                .lock()
-                .unwrap()
-                .get(&args.into_inner().body[..])
-                .unwrap()
-                .to_vec();
+            let val = db.lock().await.get(&body[..]).unwrap().to_vec();
             // Size each frame upto MAX_BYTE_SIZE and encapsulate in response packet
             for i in 0..val.len() / MAX_BYTE_SIZE {
                 tx.send(Ok(Byte {
@@ -162,12 +157,12 @@ impl Dstore for Global {
         let key = args.into_inner().body;
 
         // Push KEY into invalidate queue of all node
-        for addr in self.cluster.lock().unwrap().values() {
-            addr.lock().unwrap().push_back(Bytes::from(key.clone()));
+        for addr in self.cluster.lock().await.values() {
+            addr.lock().await.push_back(Bytes::from(key.clone()));
         }
 
         // Remove KEY mapping from Global
-        match self.db.lock().unwrap().remove(&key[..]) {
+        match self.db.lock().await.remove(&key[..]) {
             Some(_) => Ok(Response::new(Null {})),
             None => Err(Status::not_found(format!(
                 "Couldn't remove {}",
@@ -176,18 +171,18 @@ impl Dstore for Global {
         }
     }
 
-    /// RPC to help Local invalidate cached VALUES
+    /// RPC to help Local invalidate cached VALUEs
     async fn update(&self, args: Request<Byte>) -> Result<Response<Byte>, Status> {
         // Extract and return a KEY from invalidate queue associated with requesting Local
-        let args = args.into_inner().body;
+        let Byte { body } = args.into_inner();
         match self
             .cluster
             .lock()
-            .unwrap()
-            .get(&args[..])
+            .await
+            .get(&body[..])
             .unwrap()
             .lock()
-            .unwrap()
+            .await
             .pop_front()
         {
             Some(keys) => Ok(Response::new(Byte {
@@ -197,4 +192,3 @@ impl Dstore for Global {
         }
     }
 }
-
